8aabd3911ff22310aedd58ee177cd7f7151a099e
khalidahmed <khalidahmedhu@gmail.com>
2020-01-13 19:50:55 +0200

    updated time picker with simple design and more interactive controlers

--- assets/js/timepicker.js
+++ assets/js/timepicker.js
@@ -6,13 +6,18 @@ $(document).ready(function(){
             <div class='timeSelectorWrapper'>\
                 <span class='closeTimePicker'>x</span>\
                 <div class='row controlloersWrapper'>\
-                    <div class='col-xs-6 form-group MintsWrapper'>\
+                    <div class='col-xs-4 form-group MintsWrapper'>\
                         <span class='mintsIncrease'>+</span>\
                         <input  class='mintsWrapper' value='0'>\
                         <span class='mintsDecrease'>-</span>\
                     </div> \
-                    <div class='col-xs-6 form-group HoursWrapper'> \
+                    <div class='col-xs-4 form-group HoursWrapper'> \
                         <span class='hoursIncreas'>+</span> \
+                        <input class='hoursWrapper' value='1'> \
+                        <span class='hoursDecrease'>-</span> \
+                    </div>\
+                    <div class='col-xs-4 form-group HoursWrapper'> \
+                        <span class='TimeType'>+</span> \
                         <input class='hoursWrapper' value='1'> \
                         <span class='hoursDecrease'>-</span> \
                     </div>\
@@ -25,20 +30,29 @@ $(document).ready(function(){
     });
     $("#dynamicRowsWrapper").on('focus','.timepickerInput',function(){
         $(this).siblings(".timeSelectorWrapper").slideDown();
+        $(this).parent(".timepickerInputWrapper").parent("td").parent("tr").siblings("tr").children("td").children(".timepickerInputWrapper").children(".timeSelectorWrapper").slideUp();
     });
-    $("#dynamicRowsWrapper").on('click','.closeTimePicker',function(){
-        $(this).parent(".timeSelectorWrapper").slideUp();
-        let timeInput = $(this).parent(".timeSelectorWrapper").siblings(".timepickerInput"),
-            hours = $(this).siblings(".controlloersWrapper").children(".HoursWrapper").children(".hoursWrapper").val(),
-            mints = $(this).siblings(".controlloersWrapper").children(".MintsWrapper").children(".mintsWrapper").val();
-            if (hours > 0 && hours < 25 ) {
+    $("#dynamicRowsWrapper").on('click','.timeTypeOption',function(){
+        $(this).addClass("selectedOption").siblings(".timeTypeOption").removeClass("selectedOption");
+    });
+    $("#dynamicRowsWrapper").on('click','.cancelTime',function(){
+        $(this).parent("div").parent(".timeSelectorWrapper").slideUp();
+    });
+
+    $("#dynamicRowsWrapper").on('click','.registerTime',function(){
+        $(this).parent("div").parent(".timeSelectorWrapper").slideUp();
+        let timeInput = $(this).parent("div").parent(".timeSelectorWrapper").siblings(".timepickerInput"),
+            hours = $(this).parent("div").siblings(".controlloersWrapper").children(".HoursWrapper").children(".hoursWrapper").val(),
+            mints = $(this).parent("div").siblings(".controlloersWrapper").children(".MintsWrapper").children(".mintsWrapper").val(),
+            type = $(this).parent("div").siblings(".controlloersWrapper").children(".TimeTypeWrapper").children(".selectedOption").data('value');
+            if (hours > 0 && hours < 13 ) {
                 if (parseInt(mints) > -1 && parseInt(mints) < 61) {
-                    timeInput.val( mints + " : " + hours );
+                    timeInput.val( mints + " : " + hours + " " + type );
                 }
             }
     });
     $("#dynamicRowsWrapper").on('click','.hoursIncreas',function(){
-        if (  parseInt($(this).siblings(".hoursWrapper").val()) <= 23 ){
+        if (  parseInt($(this).siblings(".hoursWrapper").val()) <= 11 ){
             $(this).siblings(".hoursWrapper").val( parseInt($(this).siblings(".hoursWrapper").val())+1);
         }
     });
